1ec85198f72a4bb72ff69136e34983513c9e007b
Osnago-L <osna.brera@gmail.com>
2021-12-10 16:31:43 +0100

    end project

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,5 +2,19 @@
 for(i=1;i<=100;i++){
     console.log(i)
     let divn = document.createElement("div")
-    document.querySelector("flex").append(divn)
+    divn.className = "square"
+    document.querySelector(".flex").append(divn)
+
+    if((i%3==0)&&(i%5==0)){
+        divn.innerHTML = "FIZZBUZZ"
+        divn.classList.add("fizzbuzz_bc")
+    }else if(i%5==0){
+        divn.innerHTML = "BUZZ"
+        divn.classList.add("buzz_bc")
+    }else if(i%3==0){
+        divn.innerHTML = "FIZZ"
+        divn.classList.add("fizz_bc")
+    }else{
+        divn.innerHTML = i
+    }
 }
